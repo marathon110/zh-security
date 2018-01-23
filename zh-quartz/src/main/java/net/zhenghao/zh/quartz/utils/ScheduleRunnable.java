b94800d44f5e559e8b907725d4afeb46f7e8aa85
zhaozhenghao <736720794@qq.com>
2018-01-23 09:58:20 +0800

    注释定时任务runnable

--- zh-quartz/src/main/java/net/zhenghao/zh/quartz/utils/ScheduleRunnable.java
+++ zh-quartz/src/main/java/net/zhenghao/zh/quartz/utils/ScheduleRunnable.java
@@ -40,6 +40,7 @@ public class ScheduleRunnable implements Runnable {
 	public void run() {
 		try {
 			//这里具体的实例化是利用cglib类库，通过Java反射原理，构造函数实例化方式实例化
+			//让给定的方法可以访问，如果需要，可以显式地设置它。
 			ReflectionUtils.makeAccessible(method);
 			if (StringUtils.isNotBlank(params)) {
 				method.invoke(target, params);
@@ -50,5 +51,12 @@ public class ScheduleRunnable implements Runnable {
 			throw new RRException("执行定时任务失败", e);
 		}
 	}
+	/**
+	 * JAVA反射机制是在运行状态中，对于任意一个类，都能够知道这个类的所有属性和方法；对于任意一个对象，
+	 * 都能够调用它的任意一个方法；这种动态获取的信息以及动态调用对象的方法的功能称为java语言的反射机制。
+	 * 
+	 * Java反射机制主要提供了以下功能： 在运行时判断任意一个对象所属的类；在运行时构造任意一个类的对象；
+	 * 在运行时判断任意一个类所具有的成员变量和方法；在运行时调用任意一个对象的方法；生成动态代理。
+	 */
 
 }
