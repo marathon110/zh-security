0b652ff6779f40288fbd7b9d513f7c0930854d54
zhaozhenghao <736720794@qq.com>
2018-01-25 16:56:55 +0800

    更换XSS工具类，使其支持富文本标签提交，并拦截危险html标签

--- zh-common/src/main/java/net/zhenghao/zh/common/xss/XssHttpServletRequestWrapper.java
+++ zh-common/src/main/java/net/zhenghao/zh/common/xss/XssHttpServletRequestWrapper.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletRequestWrapper;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
@@ -28,7 +30,7 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 	//没被包装过的HttpServletRequest(特殊场景，需要自己过滤)
 	HttpServletRequest orgRequest;
 	//html过滤
-	private final static HTMLFilter htmlFilter = new HTMLFilter();
+	//private final static HTMLFilter htmlFilter = new HTMLFilter();
 	
 	public XssHttpServletRequestWrapper(HttpServletRequest request) {
 		super(request);
@@ -130,7 +132,8 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     }
 	
 	private String xssEncode(String input) {
-		return htmlFilter.filter(input);
+		//return htmlFilter.filter(input);
+		return Jsoup.clean(input, Whitelist.relaxed());//content:要处理的内容
 	}
 	
 	/**
@@ -151,5 +154,23 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
         return request;
     }
+    
+    /**
+     * 问题场景：在网页页面的富文本编辑框添加内容是我们常见的操作，在编辑框中可以给内容填色、变字体等等之后，把内容保存到数据库。
+     * 但是稍微专业的人会通过firebug/fiddler等工具拦截请求，进行修改数据(添加<script>alert(1);</script><input>等标签)，
+     * 提交到数据库保存，之后加载显示数据时浏览器就执行这些标签。所以我们要在后台处理非正常手段输入的标签内容 
+     * 
+     * jsoup 使用一个 Whitelist 类用来对 HTML 文档进行过滤，该类提供几个常用方法：  
+     * none():只允许包含文本信息  
+     * basic():允许的标签包括：a, b, blockquote, br, cite, code, dd, dl, dt, em, i, li, ol, p, pre, q, small, strike, strong, sub, sup, u, ul, 以及合适的属性  
+     * simpleText():只允许 b, em, i, strong, u 这些标签  
+     * basicWithImages():在 basic() 的基础上增加了图片  
+     * relaxed():这个过滤器允许的标签最多，包括：a, b, blockquote, br, caption, cite, code, col, colgroup, dd, dl, dt, em, h1, h2, h3, h4, h5, h6, i, img, li, ol, p, pre, q, small, strike, strong, sub, sup, table, tbody, td, tfoot, th, thead, tr, u, ul  
+     * 如果这五个过滤器都无法满足你的要求呢，例如你允许用户插入 flash 动画，没关系，Whitelist 提供扩展功能，
+     * 例如 whitelist.addTags("embed","object","param","span","div"); 也可调用 addAttributes 为某些元素增加属性。  
+     * 用法：Jsoup.clean(content, Whitelist.relaxed());//content:要处理的内容  
+     * 
+     * 此外，jsoup还有很多强大的功能 参考：  http://871421448.iteye.com/blog/1545996  jsoup 官方网站：http://jsoup.org  
+     */
 
 }
